c803425ccdd4128fe993d14fcc6007be850569e0
githubdemarco <48102206+githubdemarco@users.noreply.github.com>
2019-10-07 14:27:59 -0400

    Create entramado.c

---
+++ software/entramado.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+
+    int entramado(char posicion, int sonar, int lidar, char Data[]){    
+        
+        Data[0] = (posicion&0b01111111);
+        
+        Data[1] = (sonar&0b0000000001111111)|0b0000000010000000;
+        
+        Data[2] = (((sonar<<5)&0b0000000001100000)|((lidar>>7)&0b0000000000011111)))|0b00000000010000000;
+        
+        Data[3] = (lidar&0b0000000001111111)|0b0000000010000000;
+        
+    }
+
+
+    return 0;
+}
