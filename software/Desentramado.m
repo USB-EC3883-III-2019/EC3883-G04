1a39986768d1bdc5579bafe078c38312a85d3bfc
githubdemarco <48102206+githubdemarco@users.noreply.github.com>
2019-10-14 12:59:57 -0400

    Update Desentramado.m

--- software/Desentramado.m
+++ software/Desentramado.m
@@ -2,7 +2,7 @@ global puerto; %Crea el objeto puerto
 puerto=serial('COM7','BaudRate',115200,'Terminator','LF');   %Configutaciond e los parametros del puerto
 fopen(puerto); %abre el puerto
 
-    dac=3/(2^16); %Factor de conversión del ADC del microprocesador
+    dac=3/(2^16); %Factor de conversiÃ³n del ADC del microprocesador
 
     aux=fread(puerto,[1,buffersize],'uint8'); 
         %flush del puerto
@@ -26,10 +26,14 @@ fopen(puerto); %abre el puerto
        Numero_de_Pasos = bin(i,3:3);
        
        alfa = Numero_de_Pasos*3.5;
-       X_Sonar = Medida_Sonar*sin(alfa);
-       Y_Sonar = Medida_Sonar*cos(alfa);
-       X_Lidar = Medida_Lidar*sin(alfa);
-       Y_Lidar = Medida_Lidar*cos(alfa);
+       X_Sonar = bin2dec(Medida_Sonar)*sin(alfa);
+       Y_Sonar = bin2dec(Medida_Sonar)*cos(alfa);
+       X_Lidar = bin2dec(Medida_Lidar)*sin(alfa);
+       Y_Lidar = bin2dec(Medida_Lidar)*cos(alfa);
+       stem(X_Sonar,Y_Sonar);
+       hold on
+       stem(X_Lidar,Y_Lidar);
+       hold off
        i=i+5;
        
        %A;adido de puntos a la grafica
