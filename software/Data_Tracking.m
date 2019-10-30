31bb5a576716e77307e8dba03ff9bd6f9606b155
githubdemarco <48102206+githubdemarco@users.noreply.github.com>
2019-10-29 21:03:54 -0400

    Update Data_Tracking.m

--- software/Data_Tracking.m
+++ software/Data_Tracking.m
@@ -1,10 +1,10 @@
 global puerto; %Crea el objeto puerto
 puerto=serial('COM8','BaudRate',9800,'Terminator','LF');   %Configutaciond e los parametros del puerto
 fopen(puerto); %abre el puerto
-
+muestra = 300;
   
-    aux=fread(puerto,[1,300],'uint8'); 
-    posiciones = zeros(1,300);
+    aux=fread(puerto,[1,muestra,'uint8'); 
+    posiciones = zeros(1,muestra);
     i=1;
     
     
@@ -14,7 +14,7 @@ fopen(puerto); %abre el puerto
  
 
     bin=dec2bin(aux);    
-    while i<(290)
+    while i<(muestra-10)
 
        Numero_de_Pasos = bin(i,3:8);
        posiciones(i) = bin2dec(Numero_de_Pasos);
