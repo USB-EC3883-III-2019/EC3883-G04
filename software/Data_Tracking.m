55d44d48ca8b1ba1e1698d65ec2a9c714fe46ede
githubdemarco <48102206+githubdemarco@users.noreply.github.com>
2019-10-29 21:44:57 -0400

    Update Data_Tracking.m

--- software/Data_Tracking.m
+++ software/Data_Tracking.m
@@ -2,7 +2,7 @@ global puerto; %Crea el objeto puerto
 puerto=serial('COM8','BaudRate',9800,'Terminator','LF');   %Configutaciond e los parametros del puerto
 fopen(puerto); %abre el puerto
 muestra = 600;
-s.InputBufferSize = muestra;   
+puerto.InputBufferSize = muestra;   
     aux=fread(puerto,[1,muestra],'uint8'); 
     posiciones = zeros(1,muestra);
     i=1;
