688b6ae21d9072764c57735eacda34af21733d9e
githubdemarco <48102206+githubdemarco@users.noreply.github.com>
2019-12-02 13:00:27 -0400

    Add files via upload

--- software/Proyecto 2/Untitled.m
+++ software/Proyecto 2/Untitled.m
@@ -7,17 +7,18 @@ b3 = strcat('00',zonas(3,:),zonas(1,:))
 
 %ASCII
 message = ''
-aux = hex2dec(message)
-aux2 = dec2bin(aux)
-class(aux2)
-l = length(aux2)
-mesax = aux2
+aux = dec2bin(message)
+class(aux)
+l = length(aux)
+mesax = aux
 for c = 1:8-l
     mesax = strcat('0',mesax)
 end
+
+%%
+%BIN
 clear all 
 clc
-%BIN
 message = '0101'
 
 l = length(message)
@@ -27,5 +28,21 @@ if l<8
         mesax = strcat('0',mesax)
     end
 else
-    message = message(1:8)
+    mesax = message(1:8)
+end
+%%
+%DEC
+clear all 
+clc
+message = '1'
+aux = str2num(message)
+if aux >255||aux<0
+    aux = 255
+end
+aux2 = dec2bin(aux)
+
+l = length(aux2)
+mesax = aux2
+for c = 1:8-l
+    mesax = strcat('0',mesax)
 end
